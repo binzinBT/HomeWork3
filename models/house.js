a70baea46d0b46aaecb45ecc66f0b7962fd06bcb
Fred <binzin@bk.ru>
2018-01-12 12:54:12 +0200

    01.12

--- models/house.js
+++ models/house.js
@@ -10,16 +10,17 @@ class House {
   };
 
   removeRoom (nameRoom) {
-    for (let i = 0; i < this._listRoom.length; i++){
+    /*for (let i = 0; i < this._listRoom.length; i++){
       if (this._listRoom[i]._room._name === nameRoom){
         this._listRoom.splice( i, 1);
         break;
       }
-    }
+    }*/
+    this._listRoom = this._listRoom.filter((elem)=>{return  elem._room._name !== nameRoom});
   };
 
   removeDevice (idRoom, idDevice) {
-    for (let i = 0; i < this._listRoom.length; i++){
+    /*for (let i = 0; i < this._listRoom.length; i++){
       if (this._listRoom[i]._room._id === idRoom){
         for (let j = 0; j < this._listRoom[i]._room._listDevice.length; j++){
           if (this._listRoom[i]._room._listDevice[j]._id === idDevice){
@@ -29,7 +30,16 @@ class House {
         }
         break;
       }
-    }
+    }*/
+
+    this._listRoom.forEach((item) => {
+      if (item._room.getIdRoom() === idRoom) {
+        item._room._listDevice = item._room._listDevice.filter((elem) => {
+          return elem._id !== idDevice;
+        });
+      }
+    });
+
   };
 
 }
